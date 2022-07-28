5be88b88fffb1cb195b8e7988305ec0c0d18403a
radosevic-dejan <radosevic.dejan@proton.me>
2022-07-28 19:11:13 +0200

    Small footer correction

--- src/components/footer/footerIcons.jsx
+++ src/components/footer/footerIcons.jsx
@@ -4,11 +4,11 @@ const icons = ["facebook", "instagram", "github"];
 
 export const FooterIcons = () => {
   return (
-    <div className="w-[80%] mx-auto rounded-md flex justify-around content-center my-4 bg-[#2e3b46] text-2xl">
+    <div className="w-[80%] mx-auto rounded-md flex justify-center content-center my-4 bg-[#2e3b46] text-2xl py-2">
         {
             icons.map((icon, index) => {
                 return (
-                    <Icon key={index} iconType={icon} className=" text-slate-300 hover:text-[#fa6763]"/>
+                    <Icon key={index} iconType={icon} className=" text-slate-300 hover:text-[#fa6763] mx-4"/>
                 )
             })
         }
